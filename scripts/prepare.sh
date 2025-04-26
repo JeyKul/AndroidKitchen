66952cc4717916255a07cb8439d549abca717bb0
JeyKul <jey@kulovac.de>
2025-04-26 21:45:57 +0200

    PREPARE | ADD BINARIES FOR SIMGs AND SUPER PARTITIONS (UNFINISHED)

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # MESSAGE #
 echo "setting up workspace "
 
@@ -8,3 +10,22 @@ mkdir -p $WKSPCE
 mkdir -p $PRJCT $DWNLD
 
 mkdir -p $LOG
+
+# DOWNLOAD LPUNPACK AND SIMG2IMG #
+
+mkdir -p $WKSPCE/bin
+
+wget -N https://github.com/unix3dgforce/lpunpack/raw/refs/heads/master/lpunpack.py -O $WKSPCE/bin/lpunpack
+
+chmod +x $WKSPCE/bin/lpunpack
+
+PATH=$WKSPCE/bin:$PATH
+
+
+LATEST_URL=$(curl -s https://api.github.com/repos/JeyKul/android-simg2img/releases/latest | jq -r '.assets[] | select(.name | endswith(".tar.gz")) | .browser_download_url')
+
+wget -q $LATEST_URL -O /tmp/simg_tools.tar.gz && \
+mkdir -p $WKSPCE/bin && \
+tar -xzf /tmp/simg_tools.tar.gz -C $WKSPCE/bin/
+
+rm /tmp/simg_tools.tar.gz
